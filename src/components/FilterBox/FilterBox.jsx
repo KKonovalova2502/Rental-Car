9393bf8596ac6c1cabba3b5c16ec975ee78fb2e7
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-10-04 01:14:28 +0300

    added reser button for FilterBox

--- src/components/FilterBox/FilterBox.jsx
+++ src/components/FilterBox/FilterBox.jsx
@@ -3,12 +3,19 @@ import css from './FilterBox.module.css';
 import PriceSelect from '../PriceSelect/PriceSelect.jsx';
 import MileageInputs from '../MileageInputs/MileageInputs.jsx';
 import BrandSelect from '../BrandSelect/BrandSelect.jsx';
-import { setFilters } from '../../redux/filter/slice.js';
+import { clearFilters, setFilters } from '../../redux/filter/slice.js';
 import { clearCars } from '../../redux/catalog/slice.js';
 import { selectAllFilters } from '../../redux/filter/selectors.js';
 import { useEffect, useState } from 'react';
 import { fetchCars } from '../../redux/catalog/operations.js';
 import { cleanFilters } from '../../utils/cleanFilters.js';
+
+const initialLocalFilters = {
+  brand: '',
+  rentalPrice: '',
+  minMileage: '',
+  maxMileage: '',
+};
 
 export default function FilterBox() {
   const dispatch = useDispatch();
@@ -35,6 +42,20 @@ export default function FilterBox() {
 
     dispatch(clearCars());
     dispatch(fetchCars({ page: 1, ...cleanFilters(localFilters) }));
+  };
+
+  const handleReset = () => {
+    // скидаємо локальні фільтри
+    setLocalFilters(initialLocalFilters);
+
+    // скидаємо Redux-фільтри
+    dispatch(clearFilters());
+
+    // очищаємо список машин
+    dispatch(clearCars());
+
+    // запит без фільтрів
+    dispatch(fetchCars({ page: 1 }));
   };
 
   return (
@@ -66,9 +87,14 @@ export default function FilterBox() {
         />
       </div>
 
-      <button type="submit" className={css.searchBtn}>
-        Search
-      </button>
+      <div className={css.buttonGroup}>
+        <button type="submit" className={css.searchBtn}>
+          Search
+        </button>
+        <button type="button" className={css.resetBtn} onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </form>
   );
 }
