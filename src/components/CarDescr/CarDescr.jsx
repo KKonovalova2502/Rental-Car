bf017137c5f380fb032c95003a8d9202017c1a74
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-10-05 22:32:25 +0300

    added carId on CarDetailsPage

--- src/components/CarDescr/CarDescr.jsx
+++ src/components/CarDescr/CarDescr.jsx
@@ -1,8 +1,16 @@
 import css from './CarDescr.module.css';
 
 export default function CarDescr({ car }) {
-  const { brand, model, year, address, mileage, rentalPrice, description } =
-    car;
+  const {
+    img,
+    brand,
+    model,
+    year,
+    address,
+    mileage,
+    rentalPrice,
+    description,
+  } = car;
 
   const formattedMileage =
     mileage?.toLocaleString('en-US').replace(/,/g, ' ') || '—';
@@ -10,11 +18,12 @@ export default function CarDescr({ car }) {
   const parts = address?.split(',') || [];
   const city = parts[1]?.trim() || '';
   const country = parts[2]?.trim() || '';
+  const shortId = img.match(/\/(\d+)-ai\.jpg$/)[1];
 
   return (
     <div className={css.wrapper}>
       <h2 className={css.title}>
-        {brand} {model}, {year}
+        {brand} {model}, {year} <span className={css.carId}>Id: {shortId}</span>
       </h2>
 
       <div className={css.info}>
